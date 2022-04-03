fac69627200d57e975a269866a067c3edbbd73d2
ANMOL SINGH RAJPUT <80009167+rajput8969@users.noreply.github.com>
2022-04-03 23:48:22 +0530

    Update Program.cs

--- Algorithm Programs/Program.cs
+++ Algorithm Programs/Program.cs
@@ -1,22 +1,63 @@
 using System;
-namespace BubbleSort {
-   class MySort {
-      static void Main(string[] args) {
-         int[] arr = { 78, 55, 45, 98, 13 };
-         int temp;
-         for (int j = 0; j <= arr.Length - 2; j++) {
-            for (int i = 0; i <= arr.Length - 2; i++) {
-               if (arr[i] > arr[i + 1]) {
-                  temp= arr[i + 1];
-                  arr[i + 1] = arr[i];
-                  arr[i] = temp;
-               }
-            }
+namespace Merge {
+   class Example {
+      static public void merge(int[] arr, int p, int q, int r) {
+         int i, j, k;
+         int n1 = q - p + 1;
+         int n2 = r - q;
+         int[] L = new int[n1];
+         int[] R = new int[n2];
+         for (i = 0; i < n1; i++) {
+            L[i] = arr[p + i];
          }
-         Console.WriteLine("Sorted:");
-         foreach (int p in arr)
-            Console.Write(p + " ");
-         Console.Read();
+         for (j = 0; j < n2; j++) {
+            R[j] = arr[q + 1 + j];
+         }
+         i = 0;
+         j = 0;
+         k = p;
+         while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+               arr[k] = L[i];
+               i++;
+            } else {
+               arr[k] = R[j];
+               j++;
+            }
+            k++;
+         }
+         while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+         }
+         while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+         }
+      }
+      static public void mergeSort(int[] arr, int p, int r) {
+         if (p < r) {
+            int q = (p + r) / 2;
+            mergeSort(arr, p, q);
+            mergeSort(arr, q + 1, r);
+            merge(arr, p, q, r);
+         }
+      }
+      static void Main(string[] args) {
+         int[] arr = {76, 89, 23, 1, 55, 78, 99, 12, 65, 100};
+         int n = 10, i;
+         Console.WriteLine("Merge Sort");
+         Console.Write("Initial array is: ");
+         for (i = 0; i < n; i++) {
+            Console.Write(arr[i] + " ");
+         }
+         mergeSort(arr, 0, n-1);
+         Console.Write("\nSorted Array is: ");
+         for (i = 0; i < n; i++) {
+            Console.Write(arr[i] + " ");
+         }
       }
    }
 }

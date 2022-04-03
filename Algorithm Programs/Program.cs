dc273c0b6300903d6f08f5be3ddc0dcbf17c9e66
ANMOL SINGH RAJPUT <80009167+rajput8969@users.noreply.github.com>
2022-04-03 23:33:50 +0530

    Update Program.cs
    Adding Swap

--- Algorithm Programs/Program.cs
+++ Algorithm Programs/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+using System;
 
 namespace Algorithm_Programs
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void permute(String str,
+                                int l, int r)
         {
-            Console.WriteLine("Hello World!");
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+
+        /**
+        * Swap Characters at position
+        * @param a string value
+        * @param i position 1
+        * @param j position 2
+        * @return swapped string
+        */
+        public static String swap(String a,
+                                int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+        // Driver Code
+        public static void Main()
+        {
+            String str = "ABC";
+            int n = str.Length;
+            permute(str, 0, n - 1);
         }
     }
 }
